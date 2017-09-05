154890e643bd2ce3e82b05f56c0fae93ea330533
Mark <mark@bhalash.com>
2017-09-05 09:38:51 +0100

    Code formatting cleanup.

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
-"use strict";
+'use strict';
 // MIT License btw HaHAA
 
 function createCard(cardType, mouseEvent) {
-	const origAudio = document.getElementById(cardType+"-audio");
-	const newAudio = origAudio.cloneNode()
-	const origImage = document.getElementById(cardType+"-image");
-	const newImage = origImage.cloneNode();
+    const origAudio = document.getElementById(cardType + '-audio');
+    const newAudio = origAudio.cloneNode();
+    const origImage = document.getElementById(cardType + '-image');
+    const newImage = origImage.cloneNode();
+    const body = document.getElementsByTagName('body')[0];
+    const card = { width: 198, height: 143 };
 
-	newAudio.play()
+    newAudio.play();
 
-	newImage.style.top = (mouseEvent.clientY - 198) + "px";
-	newImage.style.left = (mouseEvent.clientX - 143) + "px";
-	newImage.classList.remove("preload");
-	const body = document.getElementsByTagName("body")[0];
-	body.appendChild(newImage);
+    newImage.style.top = (mouseEvent.clientY - card.width) + 'px';
+    newImage.style.left = (mouseEvent.clientX - card.height) + 'px';
+    newImage.classList.remove('preload');
+    body.appendChild(newImage);
 }
 
 function click(mouseEvent) {
-	const percentChanceSquire = 0.25;
-	let cardType;
-	if (Math.random() < 0.25) {
-		cardType = "squire";
-	} else {
-		cardType = "knight";
-	}
-	createCard(cardType, mouseEvent);
+    const percentChanceSquire = 0.25;
+
+    if (Math.random() < percentChanceSquire) {
+        createCard('squire', mouseEvent);
+    } else {
+        createCard('knight', mouseEvent);
+    }
 }
 
-document.addEventListener("click", click);
+document.addEventListener('click', click);
